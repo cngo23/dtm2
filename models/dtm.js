3dab20c09756fc85fad1446146996b0d541df069
Christian Ngo <ngochristian23@gmail.com>
2018-05-15 15:56:03 -0500

    models for testscores and dtm

--- models/dtm.js
+++ models/dtm.js
@@ -1,11 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
     var student = sequelize.define("students", {
-        text:{type: DataTypes.STRING,
-        validate:{
-            len:[3,25]
-        },
-    },
-    flag: DataTypes.BOOLEAN
+       student_id: {
+           type: DataTypes.INTEGER,
+           autoIncrement: true,
+           primaryKey: true
+       },
+       name: {
+           type: DataTypes.STRING,
+           notEmpty: true
+       }
+       
     })
 
     return student;
