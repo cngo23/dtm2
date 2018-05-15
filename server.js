c0b7548eb4f81d8826e63c2921a6a7d8648bd4d7
Christian Ngo <ngochristian23@gmail.com>
2018-05-15 18:20:31 -0500

    more adds i forgot about

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require("./controllers/dtmcontroller.js");
 var app = express();
 var exphbs = require("express-handlebars");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use("/", routes)
 app.use(express.static(path.join(__dirname, "public")));
@@ -21,7 +21,7 @@ var PORT = 4242;
 
 var db = require("./models");
 
-db.sequelize.sync({}).then(function() {
+db.sequelize.sync({force:true}).then(function() {
     app.listen(process.env.PORT || PORT, function () {
         console.log("Server listening on: http://localhost: " + PORT)
     });
