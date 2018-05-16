4a9b4f0a47c4752cc0cfa5bb74cee5b0fa41be14
Your Name <juhipatel608@gmail.com>
2018-05-16 15:45:45 -0500

    gameplay page now wokring on proper handlebars file route /gameplay and not on index

--- app/routes/gameroute.js
+++ app/routes/gameroute.js
@@ -2,16 +2,16 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/', function(req, res){
-    let css = '/css/gameplay.css';
-    let js = '/js/gameplay.js';
-    let links = {css: css, js: js}
-    res.render('gameplay', links);
-})
+// router.get('/', function(req, res){
+//     let css = '/css/gameplay.css';
+//     let js = '/js/gameplay.js';
+//     let links = {css: css, js: js}
+//     res.render('gameplay', links);
+// })
 
 router.get('/gameplay', function(req, res){
-    let css = 'public\css\gameplay.css';
-    let js = 'public\js\gameplay.js';
+    let css = '/css/gameplay.css';
+    let js = '/js/gameplay.js';
     let links = {css: css, js: js}
     res.render('gameplay', links);
 });
