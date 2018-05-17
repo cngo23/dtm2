912503cd5f9ecc9c49a042aecd62308138ce686c
Your Name <juhipatel608@gmail.com>
2018-05-17 15:11:55 -0500

    added some css fun for UI

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var env = require("dotenv").load();
 
 var models = require("./app/models");
 
-// var controllers = require("./app/controllers");
+
 
 // Static directory
 app.use(express.static('app/public'));
@@ -33,21 +33,21 @@ let routes = require('./app/routes/gameroute');
 app.use(routes);
 
 //setting pssport authentication steps
-app.use(session({secret:"dtmcat", resave: true, saveUninitialized:true})); //session secret
-app.use(passport.initialize());
-app.use(passport.session()); 
+// app.use(session({secret:"dtmcat", resave: true, saveUninitialized:true})); //session secret
+// app.use(passport.initialize());
+// app.use(passport.session()); 
 
-require("./app/routes/auth")(app, passport);
+// require("./app/routes/auth")(app, passport);
 
-require("./app/config/passport/passport")(passport, models.user);
+// require("./app/config/passport/passport")(passport, models.user);
 
-models.sequelize.sync({force:true}).then(function() {
-    console.log("Database is all gravy");
-}).catch(function(err) {
-    console.log(err, "Something went wrong with the Database Update...")
-});
+// models.sequelize.sync({force:true}).then(function() {
+//     console.log("Database is all gravy");
+// }).catch(function(err) {
+//     console.log(err, "Something went wrong with the Database Update...")
+// });
 
-var controllers = require("./app/controllers");
+// var controllers = require("./app/controllers");
 
 
 app.listen(PORT, function () {
