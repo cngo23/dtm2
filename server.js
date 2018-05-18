7de75be46c6987451b8e37f3dd299e8f1d93b376
Natalie Hoben <nataliehoben@MacBook-Pro.local>
2018-05-17 19:56:33 -0500

    connected handlebars to css for login and signup

--- server.js
+++ server.js
@@ -31,23 +31,26 @@ app.set("view engine", "handlebars");
 //main routing file
 let routes = require('./app/routes/gameroute');
 app.use(routes);
-
+let loginroutes = require('./app/routes/login');
+app.use(loginroutes);
+let signuproutes = require('./app/routes/signup');
+app.use(signuproutes);
 //setting pssport authentication steps
-app.use(session({secret:"dtmcat", resave: true, saveUninitialized:true})); //session secret
-app.use(passport.initialize());
-app.use(passport.session()); 
+//app.use(session({secret:"dtmcat", resave: true, saveUninitialized:true})); //session secret
+//app.use(passport.initialize());
+//app.use(passport.session()); 
 
-require("./app/routes/auth")(app, passport);
+//require("./app/routes/auth")(app, passport);
 
-require("./app/config/passport/passport")(passport, models.user);
+//require("./app/config/passport/passport")(passport, models.user);
 
-models.sequelize.sync({force:true}).then(function() {
-    console.log("Database is all gravy");
-}).catch(function(err) {
-    console.log(err, "Something went wrong with the Database Update...")
-});
+//models.sequelize.sync({force:true}).then(function() {
+   // console.log("Database is all gravy");
+//}).catch(function(err) {
+//    console.log(err, "Something went wrong with the Database Update...")
+//});
 
-var controllers = require("./app/controllers");
+//var controllers = require("./app/controllers");
 
 
 app.listen(PORT, function () {
