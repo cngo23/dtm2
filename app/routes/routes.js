5a6f5443065a60083af98eefc9f4672e4e443117
Christian Ngo <ngochristian23@gmail.com>
2018-05-17 21:03:50 -0500

    general front end routes added and cleaned up

---
+++ app/routes/routes.js
@@ -0,0 +1,34 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+    res.render('index');
+})
+
+router.get('/login', function(req, res){
+   // let css = '/css/styles.css';
+  
+    //let loginlinks = {css: css}
+    //res.render('login', loginlinks);
+    res.render("../views/pages/login")
+});
+
+router.get('/signup', function(req, res){
+     res.render("../views/pages/signup")
+ });
+
+ router.get('/gameplay', function(req, res){
+    let css = '/css/gameplay.css';
+    let js = '/js/gameplay.js';
+    let links = {css: css, js: js}
+    res.render('gameplay', links);
+});
+
+
+module.exports = router;
+
+
+//app.get('public\css\gameplay.css', function(req, res){ res.send('public\css\gameplay.css'); res.end(); });
+//app.get('public\js\gameplay.js', function(req, res){ res.send('public\js\gameplay.js'); res.end(); });
+
